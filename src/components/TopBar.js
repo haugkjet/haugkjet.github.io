636f7c34b3afd8b600aa889ce92d24398ab52f49
Kjetil Haugland <haugkjet@gmail.com>
2022-05-04 07:56:34 +0200

    Switch beetween light/dark mode

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,7 +1,18 @@
 import { useState } from "react";
 import { MdDarkMode, MdLightMode } from "react-icons/md";
+import useLocalStorage from "use-local-storage";
 
 function TopBar() {
+  const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "light");
+
+  const switchTheme = () => {
+    const html = window.document.documentElement;
+    const newTheme = theme === "light" ? "dark" : "light";
+    html.classList.remove(theme);
+    html.classList.add(newTheme);
+    setTheme(newTheme);
+  };
+
   return (
     <nav className="flex items-center justify-between flex-wrap bg-slate-100 dark:bg-black p-6 w-auto">
       <div className="flex items-center flex-shrink-0 text-black dark:text-white mr-6">
@@ -21,6 +32,10 @@ function TopBar() {
         </div>
         <div className="flex items-center flex-shrink-0  text-black dark:text-white mr-16">
           <span className="font-semibold text-base">Contact</span>
+        </div>
+        <div className="flex items-center flex-shrink-0  text-black dark:text-white mr-16">
+          <MdDarkMode onClick={switchTheme}></MdDarkMode>
+          <MdLightMode onClick={switchTheme}></MdLightMode>
         </div>
       </div>
     </nav>
