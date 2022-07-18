16638df383a9d1df899f9f2fb2cae179192477cf
Kjetil Haugland <haugkjet@gmail.com>
2022-07-18 09:48:05 +0200

    Changed text

--- src/components/ContentArea.js
+++ src/components/ContentArea.js
@@ -69,7 +69,7 @@ function ContentArea() {
         <div className="flex-row lg:w-1/2 sm:w-screen lg:h-60 sm:h-48 h-40  hover:border-green-500 bg-white dark:bg-slate-800 shadow shadow-cyan-700 dark:shadow-cyan-300 border border-cyan-500 rounded-md m-2">
           <a href="https://haugkjet.github.io/threejs-landing-page-test/client">
             <h3 className="text-black dark:text-white text-l ml-6 font-bold">
-              ThreeJS landing page
+              ThreeJS test page
             </h3>
           </a>
         </div>
